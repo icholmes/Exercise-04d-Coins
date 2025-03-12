b32943798470e9679f8421cd815448b0a63ed628
Izzy Holmes <icholmes@iu.edu>
2025-03-12 11:16:54 -0400

    lots of changes

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("------ Audio Source ------")]
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    [Header("------Audio Clip------")]
+    public AudioClip background;
+    public AudioClip enemyDeath;
+    public AudioClip coin;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        musicSource.clip = background;
+        musicSource.Play();
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        SFXSource.PlayOneShot(clip);
+    }
+}
